9ede0fccb465cf562ac7990b8aceb33bcdbdae18
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-12-20 21:25:21 +0600

    addea

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const res = require('express/lib/response');
+// const res = require('express/lib/response');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const app = express();
 
 
 // middleware
@@ -12,12 +12,26 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pa0zg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pa0zg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://bike-gallery:6rCQawuvO3dtNvng@cluster0.pa0zg.mongodb.net/?retryWrites=true&w=majority";
+
+// var uri = "mongodb://bike-gallery:6rCQawuvO3dtNvng@cluster0-shard-00-00.pa0zg.mongodb.net:27017,cluster0-shard-00-01.pa0zg.mongodb.net:27017,cluster0-shard-00-02.pa0zg.mongodb.net:27017/?ssl=true&replicaSet=atlas-ofx4o1-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+const uri = "mongodb+srv://bike-gallery:PgX49iiexHkpj85b@cluster0.pa0zg.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 async function run() {
     try {
         await client.connect();
+
         const itemsCollection = client.db("bike-gallery").collection("item");
 
         //get item 
@@ -44,7 +58,7 @@ async function run() {
         })
 
 
-        // delete item
+        delete item
         app.delete('/item/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -98,7 +112,7 @@ async function run() {
 
 }
 console.log('mongo connect');
-run().catch(console.dir)
+run()
 
 app.get('/', (req, res) => {
     res.send('welCome To Bike Gallery')
